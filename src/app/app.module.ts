a2f0e0600f509cefa2bd44ddef4c1859bbb19f57
daniel-bar <72673770+daniel-bar@users.noreply.github.com>
2021-03-09 10:24:52 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ContactComponent } from './components/pages/contact/contact.component';
 import { ProductComponent } from './components/pages/product/product.component';
 import { RegisterComponent } from './components/pages/register/register.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { LoginComponent } from './components/pages/login/login.component';
     ProductComponent,
     RegisterComponent,
     LoginComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
