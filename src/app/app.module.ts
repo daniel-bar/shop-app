e77ad070c868624c3663cc885f7ec3a05550bdb1
daniel-bar <72673770+daniel-bar@users.noreply.github.com>
2021-03-16 10:50:26 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,12 @@ import { ProductComponent } from './components/pages/product/product.component';
 import { RegisterComponent } from './components/pages/register/register.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { ProfileComponent } from './components/pages/profile/profile.component';
+import { AccountOverviewComponent } from './components/pages/profile/account-overview/account-overview.component';
+import { MyOrdersComponent } from './components/pages/profile/my-orders/my-orders.component';
+import { MyDetailsComponent } from './components/pages/profile/my-details/my-details.component';
+import { ShoppingBagComponent } from './components/pages/shopping-bag/shopping-bag.component';
+import { SelectComponent } from './components/UI/select/select.component';
+import { CheckoutComponent } from './components/pages/shopping-bag/checkout/checkout.component';
 
 
 @NgModule({
@@ -25,6 +31,12 @@ import { ProfileComponent } from './components/pages/profile/profile.component';
     RegisterComponent,
     LoginComponent,
     ProfileComponent,
+    AccountOverviewComponent,
+    MyOrdersComponent,
+    MyDetailsComponent,
+    ShoppingBagComponent,
+    SelectComponent,
+    CheckoutComponent,
   ],
   imports: [
     BrowserModule,
